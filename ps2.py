b2d899f7f26eb02a83b0993ebf2f766f6efa38d1
terifive <tteriandan@college.harvard.edu>
2026-09-23 16:56:41 -0400

    Add files via upload

---
+++ ps2.py
@@ -0,0 +1,103 @@
+class BinarySearchTree:
+    # left: BinarySearchTree
+    # right: BinarySearchTree
+    # key: int
+    # item: int
+    # size: int
+    def __init__(self, debugger = None):
+        self.left = None
+        self.right = None
+        self.key = None
+        self.item = None
+        self._size = 1
+        self.debugger = debugger
+
+    @property
+    def size(self):
+         return self._size
+       
+     # a setter function
+    @size.setter
+    def size(self, a):
+        debugger = self.debugger
+        if debugger:
+            debugger.inc_size_counter()
+        self._size = a
+
+    ####### Part a #######
+    '''
+    Calculates the size of the tree
+    returns the size at a given node
+    '''
+    def calculate_sizes(self, debugger = None):
+        # Debugging code
+        # No need to modify
+        # Provides counts
+        if debugger is None:
+            debugger = self.debugger
+        if debugger:
+            debugger.inc()
+
+        # Implementation
+        self.size = 1
+        if self.right is not None:
+            self.size += self.right.calculate_sizes(debugger)
+        if self.left is not None:
+            self.size += self.left.calculate_sizes(debugger)
+        return self.size
+
+    '''
+    Select the ind-th key in the tree
+    
+    ind: a number between 0 and n-1 (the number of nodes/objects)
+    returns BinarySearchTree/Node or None
+    '''
+    def select(self, ind):
+        left_size = 0
+        if self.left is not None:
+            left_size = self.left.size
+        if ind == left_size:
+            return self
+        if left_size > ind and self.left is not None:
+            return self.left.select(ind)
+        if left_size < ind and self.right is not None:
+            return self.right.select(ind)
+        return None
+
+
+    '''
+    Searches for a given key
+    returns a pointer to the object with target key or None (Roughgarden)
+    '''
+    def search(self, key):
+        if self is None:
+            return None
+        elif self.key == key:
+            return self
+        elif self.key < key and self.right is not None:
+            return self.right.search(key)
+        elif self.left is not None:
+            return self.left.search(key)
+        return None
+    
+
+    '''
+    Inserts a key into the tree
+    key: the key for the new node; 
+        ... this is NOT a BinarySearchTree/Node, the function creates one
+    
+    returns the original (top level) tree - allows for easy chaining in tests
+    '''
+    def insert(self, key):
+        if self.key is None:
+            self.key = key
+        elif self.key > key: 
+            if self.left is None:
+                self.left = BinarySearchTree(self.debugger)
+            self.left.insert(key)
+        elif self.key < key:
+            if self.right is None:
+                self.right = BinarySearchTree(self.debugger)
+            self.right.insert(key)
+        self.calculate_sizes()
+        return self
